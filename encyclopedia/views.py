a1f5ad9b5e295de98b2a549dbb42733594595f47
Lars234 <larsjd@live.nl>
2021-11-26 10:17:28 +0100

    Can now add a new page, but does not yet redirect to it and does not check for correct input

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -20,6 +20,17 @@ def entry(request, entry_name):
     })
 
 def new_page(request):
+    if request.method == "POST":
+        new_title = request.POST.get('title')
+        new_content = request.POST.get('content')
+
+        # make a new md file
+        new = open(f"entries/{new_title}.md", "w")
+        new.write(new_content)
+
+        ## werkt nog niet ##
+        return entry(request, new_title)
+
     return render(request, "encyclopedia/new_page.html", {
         "entries": util.list_entries(),
         "random": random.choice(util.list_entries())
---
+++ entries/new.md
@@ -0,0 +1,7 @@
+# page
+
+## markup
+
+### markdown
+
+tets
